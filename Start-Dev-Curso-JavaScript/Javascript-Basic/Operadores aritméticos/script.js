93b173425ab2f3112368b50ffb0dfadc1295d06e
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-10 12:45:34 -0300

    10/05/23

---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Operadores aritméticos/script.js
@@ -0,0 +1,49 @@
+// + Adição 
+var a = 10;
+var b = 15; 
+
+var c = a + b;
+
+console.log(c)
+
+// - Subtração
+var a = 10;
+var b = 15; 
+
+var c = a - b;
+
+console.log(c)
+
+// * Multiplicação
+var a = 10;
+var b = 15; 
+
+var c = a * b;
+
+console.log(c)
+
+//  / Divisão
+var a = 10;
+var b = 15; 
+
+var c = a / b;
+
+console.log(c)
+
+// % Módulo: resto da  divisão
+var a = 10;
+var b = 15; 
+
+var c = a % b;
+
+console.log(c)
+
+// ++ Inclemento
+var a = 30;
+
+var b = ++a;
+
+// -- Declemento
+var a = 30;
+
+var b = --a;
---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Tipos de dados/script.js
@@ -0,0 +1,14 @@
+// São os seguintes:
+
+//Boolean: True/False
+
+//Null: Vasio
+
+//Undefined: Sem definição
+
+//String
+var a = "Palavra de Deus \n Salvação";
+// Sequências de caracteres alfanuméricos (letras, números e/ou símbolos) amplamente usadas em programação.
+// \n Quebra de linha
+
+console.log(a)
