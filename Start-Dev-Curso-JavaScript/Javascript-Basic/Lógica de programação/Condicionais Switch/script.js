aefc6b25e4c595ef6ff96cc1ba7b9cc9411c70cb
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-12 13:37:20 -0300

    120/05/23

---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Condicionais Switch/script.js
@@ -0,0 +1,16 @@
+var nota1 =  7.1;
+var nota2 =  6.9;
+
+
+var media = (nota1 + nota2) / 2;
+
+if(media >= 7){
+
+console.log("Você passou na média!");
+}
+else{
+    (media <= 7);
+
+    console.log("Você esta de recuperação!")
+}
+
---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Operador ternário/script.js
@@ -0,0 +1,8 @@
+var idade =  18
+
+
+//idade >= 18 ? console.log("Pode") : console.log("Não pode")
+
+var pode =  idade >= 18 ? true : false;
+console.log("Pode");
+
