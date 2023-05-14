327b21197de5d6dda1eef564fefaf6a8613dce39
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-14 08:55:31 -0300

    14/05/23

--- Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Condicionais Switch/script.js
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Condicionais Switch/script.js
@@ -1,10 +1,10 @@
-var nota1 =  7.1;
-var nota2 =  6.9;
+var nota1 =  8.0;
+var nota2 =  8.0;
 
 
 var media = (nota1 + nota2) / 2;
 
-if(media >= 7){
+/*if(media >= 7){
 
 console.log("Você passou na média!");
 }
@@ -12,5 +12,44 @@ else{
     (media <= 7);
 
     console.log("Você esta de recuperação!")
+*/
+
+var coceito = ""
+
+if (media >=8){
+
+    coceito = "Òtimo";
+
 }
+else if( media >= 6.5){
+
+    coceito = "Bom";
+
+} else{
+
+    coceito = "Regular";
+}
+
+console.log(media);
+console.log(coceito);
+
+switch(coceito){
+
+case "Ótimo":
+    console.log("Parabéns, vc é um excelente aluno!");
+    break;
+case "Bom":
+    console.log("Você está quase perfeito!");
+    break;
+case "Regular":
+    console.log("Vamos estudar mais um pouco?");
+    break;
+default:
+console.log("Houve algum erro");
+break;
+
+
+
+}
+
 
