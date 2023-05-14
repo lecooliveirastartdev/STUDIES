3105c956030db77cf775969da3be0b169714fac9
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-14 10:47:15 -0300

    14/05/23

---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Arrays/script.js
@@ -0,0 +1,14 @@
+//var alunos =  new Array("Igor", "Alexandro", "Beatriz");
+
+//console.log(alunos[0]);
+
+//Ex 2:
+
+var alunos = ["Igor", "Alexandro", "Beatriz", "Ana"];
+
+for(var i = 0; i < alunos.length; i++){
+    console.log[alunos];
+    
+}
+
+
---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/For e While/script.js
@@ -0,0 +1,25 @@
+/*FOR
+
+var numero = 5;
+
+
+for(var vez = 0; vez < numero; vez++){
+
+console.log("Executando o for, pela " +  vez + " vez ");
+}
+ console.log("Acabou");*/
+
+ //WHILE
+
+ var numero = 0;
+
+ while (numero <= 10){
+
+    console.log("numero " + numero);
+    numero++;
+
+
+ }
+
+ console.log("Acabou");
+  
