b3acf39f2cf69e045e12b9238b476f0ccfed1059
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-17 17:03:05 -0300

    17/0523

--- Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Objetos - Resumo/script.js
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Objetos - Resumo/script.js
@@ -15,3 +15,14 @@ function obj(nome, sobrenome)
 
 }
 console.log(this.nome, + this.sobrenome);
+
+// Exemplo de criar objeto 3
+
+function  obj(n, s) {
+
+    this.nome = n;
+    this.sobrenome = s;
+
+}
+var a = new obj("Alexandro", "Oliveira");
+console.log( a[(0) + (2)]);
