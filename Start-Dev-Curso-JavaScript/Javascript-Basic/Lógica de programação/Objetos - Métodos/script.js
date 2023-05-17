cd1c7de9af68a41292536dc4dfab0fed6f1c418e
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-17 10:59:54 -0300

    17/05/23

---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Objetos - Métodos/script.js
@@ -0,0 +1,14 @@
+var aluno = {
+    none: "Alexandro",
+    notas: [5, 8],
+
+
+    media: function (n1, n2) {
+
+        return (n1 + n1) / 2
+    }
+
+}
+
+console.log((aluno));
+
---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Objetos - Propriedades/script.js
@@ -0,0 +1,15 @@
+/*var aluno = {
+    nome: "Alexandro",
+    notas: [7.5, 8.0]
+}
+
+console.log(aluno.notas[1]);*/ 
+
+
+var aluno= {
+
+    nome: "Alexandro",
+    notas: [10, 8]
+};
+
+ console.log(aluno);
