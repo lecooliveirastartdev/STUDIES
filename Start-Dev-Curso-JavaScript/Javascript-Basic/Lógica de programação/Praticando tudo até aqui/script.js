471fb13ebb5d678c21b3d4f52fd32e05d71ee710
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-17 10:02:42 -0300

    17/05/23

--- Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Praticando tudo até aqui/script.js
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Praticando tudo até aqui/script.js
@@ -2,35 +2,50 @@
 //Nome do aluno - nota 1 - nota 2 -m~edia - apronado - reprovado
 
 
-var nomes =["Alexandro", "Beatriz", "Davi", "Ana"];
-var notasA =[7.0, 8.0, 7.5, 9.5];
-var notasB =[7.0, 7.5, 7.0, 7.5];
+var nomes = ["Alexandro", "Beatriz", "Davi", "Ana"];
+var notasA = [7.0, 8.0, 7.5, 9.5];
+var notasB = [7.0, 7.5, 7.0, 7.5];
 
 
-function media(n1, n2){
-    return(n1 + n2) / 2;
+function media(n1, n2) {
+    return (n1 + n2) / 2;
 }
 
-function passou(media){
+function passou(media) {
 
-    
+
+
+    if (media > 7) {
+        return "Aprovado";
+
+    } else {
+
+        return "Reprovado";
+    }
+
+
 
 }
 
-for(var index in nomes) {  
+for (var index in nomes) {
 
     var nota1 = notasA[index];
-    var nota2 = notasB[index];
- 
-    console.log(nomes[index] + 
-        " - " + 
-        nota1 + 
+    var nota2 = notasB[index]; 
+
+
+    var n  = media(nota1, nota2)
+
+    console.log(nomes[index] +
+        " - " +
+        nota1 +
         " - " +
         nota2 +
         " - " +
-        media(nota1, nota2 ));
+        n +
+        " - " +
+        passou(n));
 
-       
+
 }
 
 
