f2444973003f2fe1b772704db6b91bf0571b2953
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2023-05-18 10:06:34 -0300

    18/05/23

---
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Const Let e Var/script.js
@@ -0,0 +1,14 @@
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = 6;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+// Constante não pôde ser reatribuida! 
+
+//Var variável global
+
+// Let variável local
+
--- Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Datas/script.js
+++ Start-Dev-Curso-JavaScript/Javascript-Basic/Lógica de programação/Datas/script.js
@@ -3,4 +3,6 @@ var d = new Date();
 
 console.log(d);
 
-// Para refinar usamos o get + a definição escol
+// Para refinar usamos o get + a definição  de necessidade!
+// Obs: Data e um objeto.
+// set e um apontamento para uma parte especifica da data e ou hora
